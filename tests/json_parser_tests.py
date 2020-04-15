766fb95241b38b8b28006d60a45f333d6c98218a
ajarco <ajarco@chemxon.com>
2020-04-15 20:51:33 +0200

    unittest added

---
+++ tests/json_parser_tests.py
@@ -0,0 +1,39 @@
+import unittest
+import jsonparser as parser
+from jsoncompare import CompareJson
+
+class TestSum(unittest.TestCase):
+
+    def test_sum(self):
+        self.assertEqual(sum([1, 2, 3]), 6, "Should be 6")
+
+    def test_sum_tuple(self):
+        self.assertEqual(sum((1, 2, 2)), 6, "Should be 6")
+
+class TestJsonParser(unittest.TestCase):
+
+    #def __init__(self):
+    #    self.json_string="{'key1': 'value1', 'key2': 'value2'}"
+    #    self.json_dict = {'key1': 'value1', 'key2' : 'value2'}
+    #    self.parse_string = parser.ParseJson(self.json_string)
+    #    self.parse_dict = parser.ParseJson(self.json_dict)
+
+
+    # Returns True or False.
+    def test_dict_to_json(self):
+        #self.parse_string
+        #print(self.parse_dict)
+        json_dict = {"key1": "value1", "key2": "value2"}
+        parse_dict = parser.ParseJson(json_dict)
+        parse_dict.dict_to_json()
+        self.assertTrue(parse_dict.json_object["key1"] == "value1")
+
+    def test_string_to_json(self):
+        json_string= '{"key1": "value1", "key2": "value2"}'
+        parse_string = parser.ParseJson(json_string)
+        parse_string.string_to_json()
+        self.assertTrue(parse_string.json_object["key1"] == "value1")
+
+
+if __name__ == '__main__':
+    unittest.main()
